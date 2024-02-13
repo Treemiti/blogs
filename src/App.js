5c0a561258decd4947688b8311a1fea785ff71cb
Treemiti <contact@treemiti.com>
2024-02-13 19:56:29 +0530

    130224

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, createBrowserRouter,RouterProvider } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import About from './components/About';
+import Contact from './components/Contact';
+import Post from './components/Post';
+import Error from './components/Error';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <><Navigation />
+    <Header /><Main /><Footer /></>,
+  },
+  {
+    path: '/about',
+    element: <><Navigation />
+    <Header /><About /><Footer /></>,
+  },
+  {
+    path: '/post',
+    element: <><Navigation />
+    <Header /><Post /><Footer /></>,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+  {
+    path: '*',
+    element: <><Navigation />
+    <Header /><Error /><Footer /></>,
+  }
+]);
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {/* <Router> */}
+
+{/*             <Routes>
+              <Route path="" element={<Main />} />
+              <Route path="about" element={<About />} />
+              <Route path="post" element={<Post />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="*" element={<Error />} />
+            </Routes> */}
+            <RouterProvider router={router} />
+          
+        {/* </Router> */}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
